b179b48a6426d7981e32d54f3de5e03e0ea91a93
Wallace Santos <wgsantos29@gmail.com>
2018-09-07 10:15:14 -0700

    Played around with mongoose queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,37 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5b8eac34235baa090422402e';
+//
+// if(!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+//
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+//
+// Todo.findById(id).then((todo) => {
+//   if(!todo) {
+//     return console.log('ID not found');
+//   }
+//   console.log('Todo by ID: ', todo);
+// }).catch((err) => console.log(err));
+
+User.findById(id).then((user) => {
+  if(!user) {
+    console.log('No such user');
+  }
+  console.log('User by ID: ', user);
+}).catch((err) => console.log(err));
