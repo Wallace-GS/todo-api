884dc1858c2c8c28f2b56277bf0bdd6c53cdf7f3
Wallace Santos <wgsantos29@gmail.com>
2018-09-08 16:45:39 -0700

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -48,6 +48,23 @@ app.get('/todos/:id', (req, res) => {
   }).catch((err) => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+    else {
+      res.send(todo);
+    }
+  }).catch((err) => res.status(400).send());
+});
+
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 })
